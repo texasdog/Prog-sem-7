d8070feb4d0e283dafc71d79781ba4e1dcad93a7
Andrew <43204345+texasdog@users.noreply.github.com>
2022-01-14 02:36:10 +0300

    Update main.py

--- Lab 5/main.py
+++ Lab 5/main.py
@@ -1,19 +1,20 @@
 #Парсинг сайта herzen.spb.ru
-from urllib.request import urlopen #Преобразуем страницу в HTML
+
+from urllib.request import urlopen 
 from bs4 import BeautifulSoup
 import re
 
-html = urlopen('https://www.herzen.spb.ru/main/structure/inst/') #Преобразуем страницу в HTML файл
-bs = BeautifulSoup(html, "html.parser") #Преобразование кода для библиотеки BS
-nameList = bs.find('td',{'class':'block'}).children #Ищем тег td класса блок и всё что внутри
-l = re.compile("/inst/.") #Маска для регулярных выражений
-nameList = bs.findAll('a',{'class':'', 'href':l}) #Найти все теги а с ссылкой по маске l
+html = urlopen('https://www.herzen.spb.ru/main/structure/inst/') 
+bs = BeautifulSoup(html, "html.parser") 
+nameList = bs.find('td',{'class':'block'}).children 
+l = re.compile("/inst/.") 
+nameList = bs.findAll('a',{'class':'', 'href':l}) 
 
 
-data = [] #Список институтов
+data = []
 key = True
 firstName = ''
-for name in nameList: #Цикл который добавляет все нужные данные в переменную data
+for name in nameList: 
     uni = name.get_text()
     if uni == firstName:
       break
@@ -27,7 +28,7 @@ print(data)
 
 html = urlopen('https://atlas.herzen.spb.ru/faculty.php')
 bs = BeautifulSoup(html, "html.parser")
-nameList = bs.findAll('a',{'class':'alist'}) #Ищем теперь с нужным классом alist
+nameList = bs.findAll('a',{'class':'alist'})
 data2 = [] #Список кафедр
 flag = False
 for e in nameList:
@@ -56,9 +57,9 @@ for e in nameList:
         head_url = "https://atlas.herzen.spb.ru/" + head.get('href')
         head_bs = BeautifulSoup(urlopen(head_url), "html.parser")
         mail = head_bs.find('a', {'href':re.compile('@')})
-      data2.append({"dep_name":name, "head_name":head_name.replace("\n", ""), "email":mail.get('href')}) #Добавляем информацию парсинга в список
+      data2.append({"dep_name":name, "head_name":head_name.replace("\n", ""), "email":mail.get('href')}) 
 print(data2)
 
 import json
-with open('data.json', 'w', encoding="utf16") as f: #Запись в json
+with open('data.json', 'w', encoding="utf16") as f: 
     json.dump(data, f, ensure_ascii=False, indent=4)
